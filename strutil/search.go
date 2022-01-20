85d5fc2bfbc3814c3cdf0998ee6fde1d3527e7d3
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-20 20:53:33 +0300

    search: add naive search algorithm.

---
+++ strutil/search.go
@@ -0,0 +1,24 @@
+package strutil
+
+// BasicSearch returns the pattern if matched in a str.
+// Returns -1 if there's no match.
+// Implemented using the Naive searching algorithm (https://en.wikipedia.org/wiki/String-searching_algorithm#Naive_string_search).
+// This algorithm designed for smaller texts.
+func BasicSearch(str, pattern string) int {
+	var strLength int = len(str)
+	var patternLength int = len(pattern)
+
+	for i := 0; i <= strLength-patternLength; i++ {
+		var j int
+		for j = 0; j < patternLength; j++ {
+			if str[i+j] != pattern[j] {
+				break
+			}
+		}
+
+		if j == patternLength {
+			return i
+		}
+	}
+	return -1
+}
