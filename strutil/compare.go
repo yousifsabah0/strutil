c1cf6f2f76f761acbe87a3d084061ad3b0d8ec14
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-21 14:25:30 +0300

    compare: add Compare function

---
+++ strutil/compare.go
@@ -0,0 +1,9 @@
+package strutil
+
+// Compare compares two string.
+// Returns boolean value.
+// Simple and Stupid, lol.
+// I Don't think we need to do testing with this :3
+func Compare(str1, str2 string) bool {
+	return str1 == str2
+}
